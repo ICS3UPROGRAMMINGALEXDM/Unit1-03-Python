4c1e520af008847bdbef36d78580351c9b5bd83e
Ubuntu <ubuntu@ip-172-31-86-29.ec2.internal>
2022-02-17 14:39:14 +0000

    Created hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created By: Alex De Meo
+# Date: 02/17/22
+# Displays hello world wih style
+
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
